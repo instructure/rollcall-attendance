6ac2bac3faf3ec9eaa1f9622bb7c8afe190d13a5
Luis Hilario <luis.hilario@instructure.com>
2021-10-28 14:25:25 -0500

    Remove previous migrations
    Migrations to change index are removed, since IAD region is too
    large. Index was removed, and will only be handled in code.
    
    closes CAS-543
    
    Change-Id: Iecacea34705c9b571d3c17a4a8b8182933d59788
    Reviewed-on: https://gerrit.instructure.com/c/rollcall-attendance/+/277057
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Juan Leyva <juan.leyva@instructure.com>
    QA-Review: Juan Leyva <juan.leyva@instructure.com>
    Product-Review: Juan Leyva <juan.leyva@instructure.com>

--- db/migrate/20211027154640_remove_unique_index_from_statuses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUniqueIndexFromStatuses < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :statuses, [:student_id, :section_id, :class_date, :tool_consumer_instance_guid, :course_id]
-  end
-end
